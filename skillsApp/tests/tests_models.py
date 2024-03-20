6dd8dceee3f9dc52a1b2af74b62d302e4fd4c4b5
morethanatester <benpavey@yahoo.com>
2024-03-20 10:37:38 +0000

    test failed

--- skillsApp/tests/tests_models.py
+++ skillsApp/tests/tests_models.py
@@ -16,35 +16,12 @@ from django.urls import reverse
 
 #failing test case to test pipeline
         
-'''
+
 class SimpleTestCase(TestCase):
     def test_always_fails(self):
         self.assertEqual(1, 2, "This test always fails because 1 is not equal to 2")
-'''
 
 
-'''
-DEMO tests
-
-
-#Class is the blueprint(template), create an instance in memory from the blueprint with contains data
-class LogMessageTests(TestCase):
-    #create an LogMessage instance
-    def create_LogMessage(self):
-        #constructor = is the thing that runs to build the instance of the class 
-        return LogMessage.objects.create(
-            message="This is a test message",
-            log_date = timezone.now())
-
-    #create test instance of logmessage
-    def test_logmessage_creation(self):
-
-        #test the creation of the log message instance, does it actually creates an instance
-
-        log_message = self.create_LogMessage()  #Act
-        self.assertTrue(isinstance(log_message, LogMessage))
-
-'''
 
 class SecurityTest(TestCase):
     # This setup method is run before each test. It sets up a client for making requests and a test user.
